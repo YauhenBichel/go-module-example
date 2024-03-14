098aba3ac9ca611d059fe17356ac0703e68e714c
Yauhen Bichel <ccaeybi@ucl.ac.uk>
2024-03-14 15:05:16 +0000

    added error handling

--- hello/hello.go
+++ hello/hello.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"yauhen.example.com/greetings"
 )
 
 func main() {
-	message := greetings.Hello("Yauhen")
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("Yauhen")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
 }
